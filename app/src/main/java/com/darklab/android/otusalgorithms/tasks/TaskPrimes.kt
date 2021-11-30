29e7f5f7ed6c759041bcc82d2fefa31ed953ae2e
DarkLab <salih67@mail.ru>
2021-11-30 15:49:50 +0300

    простые числа

---
+++ app/src/main/java/com/darklab/android/otusalgorithms/tasks/TaskPrimes.kt
@@ -0,0 +1,10 @@
+package com.darklab.android.otusalgorithms.tasks
+
+class TaskPrimes : ITask {
+    override val rootPath: String
+        get() = "taskprimes"
+
+    override fun run(data: Array<String>): String {
+        TODO("Not yet implemented")
+    }
+}
